3fe765467b4cb76f6bc19efe45533effdb7d1884
Jack Huang <jackhxs@gmail.com>
2013-02-02 10:21:25 -0500

    stuff

--- src/com/example/aroundtheblock/MainActivity.java
+++ src/com/example/aroundtheblock/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.aroundtheblock;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.ViewGroup.LayoutParams;
 import android.view.Window;
-import android.widget.FrameLayout;
 
 public class MainActivity extends Activity {
   private Camera mCamera;
   private CameraPreview mPreview;
+  private PhotoOverlay mOverlay;
+
+  private LocationManager mLocationManager;
+  private SensorManager mSensorManager;
+  private Sensor mOrientation;
+  private Sensor mRotation;
+
+  private Location mLoc;
+  private float[] mRot;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -19,19 +36,124 @@ public class MainActivity extends Activity {
     //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    //setContentView(R.layout.activity_main);
 
     // Create an instance of Camera
     mCamera = getCameraInstance();
     System.err.println("getCameraInstance: " + mCamera);
 
     // Create our Preview view and set it as the content of our activity.
-    mPreview = new CameraPreview(this, mCamera);
+    //mPreview = (CameraPreview) findViewById(R.id.camera_preview);
 
-    FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-    PhotoOverlay overlay = new PhotoOverlay(this);
-    preview.addView(overlay);
+    //FrameLayout preview = (FrameLayout) findViewById(R.id.camera_frame_layout);
+
+    mPreview = new CameraPreview(this, mCamera);
+    //preview.addView(mPreview, 0);
+
+    mOverlay = new PhotoOverlay(this, mCamera);
+    //preview.addView(mOverlay, 1);
+
+    setContentView(mPreview);
+    addContentView(mOverlay, new LayoutParams(LayoutParams.WRAP_CONTENT,
+        LayoutParams.WRAP_CONTENT));
+
+    mLocationManager = (LocationManager) this
+        .getSystemService(Context.LOCATION_SERVICE);
+    mSensorManager = (SensorManager) this
+        .getSystemService(Context.SENSOR_SERVICE);
+
+
+    mLoc = new Location(LocationManager.NETWORK_PROVIDER);
+
+    registerLocationSensor();
+    registerOrientationSensor();
+    registerRotationSensor();
+
   }
+
+  private void registerLocationSensor() {
+
+    // Define a listener that responds to location updates
+    LocationListener locationListener = new LocationListener() {
+      public void onLocationChanged(Location location) {
+        // Called when a new location is found by the network location provider.
+        System.err.println(location.getLatitude() + ", "
+            + location.getLongitude());
+        if (location.distanceTo(mLoc) > 100)
+        {
+          mLoc = location;
+          String url = String.format("http://aroundtheblock.kanaflash.com/ar_photos?latitude=%f&longitude=%f",
+              mLoc.getLatitude(), mLoc.getLongitude());
+          new RequestTask().execute(url);
+        }
+      }
+
+      public void onStatusChanged(String provider, int status,
+          Bundle extras) {
+      }
+
+      public void onProviderEnabled(String provider) {
+      }
+
+      public void onProviderDisabled(String provider) {
+      }
+    };
+
+    mLocationManager.requestLocationUpdates(
+        LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+  }
+
+  private void registerOrientationSensor() {
+    mOrientation = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+    if (mOrientation != null) {
+      SensorEventListener listener = new SensorEventListener() {
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+          //System.err.format("azimuth: %f, pitch: %f, roll: %f\n",
+              //event.values[0], event.values[1], event.values[2]);
+        }
+
+      };
+      mSensorManager.registerListener(listener, mOrientation, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+  }
+
+  private void registerRotationSensor() {
+    mSensorManager = (SensorManager) this
+        .getSystemService(Context.SENSOR_SERVICE);
+    mRotation = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+    if (mRotation != null) {
+      SensorEventListener listener = new SensorEventListener() {
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
+
+        public void onSensorChanged(SensorEvent event) {
+          float[] R = new float[16];
+          float[] outR = new float[16];
+          SensorManager.getRotationMatrixFromVector(R, event.values);
+          SensorManager.remapCoordinateSystem(R, SensorManager.AXIS_X, SensorManager.AXIS_Z, outR);
+          mRot = outR;
+
+          //Location p = new Location(mLoc);
+          //p.setLatitude(mLoc.getLatitude() + 5);
+          //p.setLongitude(mLoc.getLongitude() + 5);
+          mOverlay.update(mRot, mLoc);
+
+          //System.err.format("rotation:\n%f %f %f\n%f %f %f\n%f %f %f\n",
+              //outR[0], outR[1], outR[2], outR[3], outR[4], outR[5], outR[6], outR[7], outR[8]);
+        }
+
+      };
+      mSensorManager.registerListener(listener, mRotation, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+  }
+
 
   //@Override
   //public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/example/aroundtheblock/PhotoOverlay.java
+++ src/com/example/aroundtheblock/PhotoOverlay.java
@@ -1,65 +1,185 @@
 package com.example.aroundtheblock;
 
-import android.content.Context;
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Point4f;
+import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
+
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
+import android.hardware.Camera;
+import android.location.Location;
+import android.view.Display;
+import android.view.View;
 
-
-public class PhotoOverlay extends SurfaceView implements SurfaceHolder.Callback {
+public class PhotoOverlay extends View {
 
   private PhotoOverlayThread mThread;
-  private int x;
+  private Camera mCamera;
+  private Activity mActivity;
+  private int mWidth, mHeight;
+  private double mXOffset, mYOffset;
 
-  public PhotoOverlay(Context context) {
-    super(context);
-    System.err.println("photOverlay");
-    getHolder().addCallback(this);
-    mThread = new PhotoOverlayThread(getHolder(), this);
+  private int x;
+  private boolean b;
+
+  public PhotoOverlay(Activity activity, Camera camera) {
+    super(activity);
+    mActivity = activity;
+    mCamera = camera;
+
+    Display display = mActivity.getWindowManager().getDefaultDisplay(); 
+    mWidth = display.getWidth();
+    mHeight = display.getHeight();
+
+    mXOffset = 0;
+    mYOffset = 0;
+    //getHolder().addCallback(this);
+    //mThread = new PhotoOverlayThread(getHolder(), this);
     setFocusable(true);
+  }
+
+
+  public boolean isPointInsideView(Camera camera, float[] R, Location m, Location p) {
+    float fov = camera.getParameters().getHorizontalViewAngle();
+
+    // apply reverse rotation to vector p-m
+    //Point4f pm = new Point4f((float)m.getLongitude(), 0, (float)m.getLatitude(), 1);
+    //Point4f pp = new Point4f((float)p.getLongitude(), 0, (float)p.getLatitude(), 1);
+
+    // x = longitude, y = latitude
+    Point4f pm = new Point4f((float)m.getLongitude(), (float)m.getLatitude(), 0, 1);
+    Point4f pp = new Point4f((float)p.getLongitude(), (float)p.getLatitude(), 0, 1);
+    pp.sub(pm);
+    Vector4f v = new Vector4f(pp);
+    Matrix4f rot = new Matrix4f(R);
+    //System.err.println(rot);
+    rot.invert();
+    Vector4f outV = new Vector4f();
+    rot.transform(v, outV);
+
+    //outV.normalize();
+    //System.err.println("pm: " + pm);
+    //System.err.println("pp: " + pp);
+    //System.err.println(outV);
+
+    // x = longitude, y = latitude, z = altitude
+    Vector4f N = new Vector4f(0, 1, 0, 0);
+    Vector4f PV = new Vector4f(outV);
+    PV.z = 0;
+    PV.normalize();
+    double theta = Math.acos(PV.dot(N));
+    theta = theta*1.46; // magic number
+
+    Vector3f N3 = new Vector3f(0, 1, 0);
+    Vector3f outV3 = new Vector3f(PV.x, PV.y, PV.z);
+    N3.cross(N3, outV3);
+    if (N3.z > 0) // negative angle
+      theta = -theta;
+
+    double thetaDeg = Math.toDegrees(theta);
+
+    double xOffset = Math.tan(theta)*((mWidth/2.0)/Math.tan(Math.toRadians(fov/2.0)));
+
+    mXOffset = xOffset;
+    //if (N3.z < 0)
+      //mXOffset = -xOffset;
+
+    //System.err.println("theta: " + thetaDeg);
+    //System.err.println("xOffset: " + mXOffset);
+
+    double vertFov = mHeight/mWidth * fov;
+    // x = longitude, y = latitude, z = altitude
+    N = new Vector4f(0, 1, 0, 0);
+    PV = new Vector4f(outV);
+    PV.x = 0;
+    PV.normalize();
+    double vertTheta = Math.acos(PV.dot(N));
+    //vertTheta = vertTheta*1.46; // magic number
+
+    N3 = new Vector3f(0, 1, 0);
+    outV3 = new Vector3f(PV.x, PV.y, PV.z);
+    N3.cross(N3, outV3);
+    if (N3.x > 0) // negative angle
+      vertTheta = -vertTheta;
+
+    //System.err.println(mWidth + " " + mHeight);
+    double vertThetaDeg = Math.toDegrees(vertTheta);
+
+    double yOffset = Math.tan(vertTheta)*((mHeight/2.0)/Math.tan(Math.toRadians(vertFov/2.0)));
+
+    mYOffset = yOffset;
+
+    if (thetaDeg > -fov/2 && thetaDeg < fov/2 &&
+        vertThetaDeg > -vertFov/2 && vertThetaDeg < vertFov/2)
+    {
+      return true;
+    }
+
+    return false;
   }
 
 
   @Override
   public void onDraw(Canvas canvas) {
+    super.onDraw(canvas); 
+
+    if (!b)
+      return;
 
     Paint paint = new Paint();
 
 
     Bitmap kangoo = BitmapFactory.decodeResource(getResources(),
         R.drawable.kangoo);
-    canvas.drawColor(Color.BLACK);
-    canvas.drawBitmap(kangoo, x, 10, null);
-    x++;
+    //canvas.drawColor(Color.BLACK);
+    //System.err.println("xOffset: " + (int)(mXOffset + mWidth/2.0));
+    canvas.drawBitmap(kangoo, (int)(mXOffset + mWidth/2.0),
+        (int)(mYOffset + mHeight/2.0), null);
+    //x++;
+
+    //Paint paint = new Paint(); 
+    //paint.setStyle(Paint.Style.FILL); 
+    //paint.setColor(Color.WHITE); 
+    //canvas.drawText("Test Text", 50, 50, paint); 
+
 
   }
 
-  public void surfaceChanged(SurfaceHolder holder, int format, int width,
-      int height) {
 
+  public void update(float[] R, Location m) {
+    Location p = new Location(m);
+    p.setLatitude(m.getLatitude() - 5);
+    b = isPointInsideView(mCamera, R, m, p);
+
+    invalidate();
   }
 
-  public void surfaceCreated(SurfaceHolder holder) {
+  //public void surfaceChanged(SurfaceHolder holder, int format, int width,
+      //int height) {
 
-    mThread.setRunning(true);
-    mThread.start();
-  }
+  //}
 
-  public void surfaceDestroyed(SurfaceHolder holder) {
+  //public void surfaceCreated(SurfaceHolder holder) {
 
-    boolean retry = true;
-    mThread.setRunning(false);
-    while (retry) {
-      try {
-        mThread.join();
-        retry = false;
-      } catch (InterruptedException e) {
-        // we will try it again and again...
-      }
-    }
-  }
+    //mThread.setRunning(true);
+    //mThread.start();
+  //}
+
+  //public void surfaceDestroyed(SurfaceHolder holder) {
+
+    //boolean retry = true;
+    //mThread.setRunning(false);
+    //while (retry) {
+      //try {
+        //mThread.join();
+        //retry = false;
+      //} catch (InterruptedException e) {
+        //// we will try it again and again...
+      //}
+    //}
+  //}
 }
